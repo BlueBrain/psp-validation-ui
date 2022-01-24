f9026d27c6d9b36994a50258791bd6e35acfb191
Stefano Antonel <stefanoantonel@gmail.com>
2022-01-24 12:37:24 +0100

    Fix plan with environments

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,7 +4,5 @@ process.env.VUE_APP_VERSION = require('./package.json').version;
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? process.env.BASE_URL
-    : '/',
+  publicPath: process.env.BASE_URL || '/',
 };
